6bd3c7e3a6b9f32db457555405f8ce43a82ead69
Ericw <eweissm1@asu.edu>
2025-03-05 15:06:23 -0700

    NewCommit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 # Load the image
 image = cv2.imread('C:/Users/Ericw/CSE598_PlanarContinuumRobotPoseEstimation/Videos/FiduicialTest_3.png')
@@ -15,10 +13,9 @@ new_height = int(image.shape[0] * scale_y)
 new_size = (new_width, new_height)
 
 resized_img = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
-bright_image = cv2.convertScaleAbs(resized_img, alpha=2.0, beta=50)  # Increase brightness
 
 # Convert the image to grayscale
-gray = cv2.cvtColor(bright_image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
 
 #load aruco dictionary
@@ -33,30 +30,12 @@ corners, ids, rejected = detector.detectMarkers(gray)
 
 # Print the detected markers
 print("Detected markers:", ids)
-Marked_image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
-# plt.figure()
-# plt.imshow(Marked_image, origin="upper")
+Marked_image = cv2.aruco.drawDetectedMarkers(resized_img, corners, ids)
 
 if ids is not None:
-#     for i in range(len(ids)):
-#         c = corners[i][0]
-#         plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label = "id={0}".format(ids[i]))
-# # plt.legend()
-# # plt.show()
-#     cv2.imshow('Detected Markers', Marked_image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-    mask = cv2.subtract(Marked_image, image)  # Find the drawn areas
-    mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    _, mask_binary = cv2.threshold(mask_gray, 1, 255, cv2.THRESH_BINARY)  # Convert to binary mask
-
-    only_markers = cv2.bitwise_and(Marked_image, Marked_image, mask=mask_binary)
-
-    # Add the markers to the original image
-    overlay = cv2.add(image, only_markers)
 
     # Show result
-    cv2.imshow('Overlayed Image', overlay)
+    cv2.imshow('Overlayed Image', Marked_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
